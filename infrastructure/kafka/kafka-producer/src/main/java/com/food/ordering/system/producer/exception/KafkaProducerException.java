bf3b560a8c2628ea203b0646d2099f7a92dcf387
dennis githinji <wakahiad@gmail.com>
2023-03-17 21:16:36 +0300

    kafka modules setup

---
+++ infrastructure/kafka/kafka-producer/src/main/java/com/food/ordering/system/producer/exception/KafkaProducerException.java
@@ -0,0 +1,7 @@
+package com.food.ordering.system.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
